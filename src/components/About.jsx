b9a6cb512cf812fd1d6c7186ac755dc6cddc12ea
wambuimuna <wambomuna@gmail.com>
2026-05-03 20:04:28 +0300

    blog

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function About({ image, about }) {
+ return (
+   <aside>
+     <img src={image} alt="blog logo" />
+     <p>{about}</p>
+   </aside>
+ );
+}
+
+
+export default About;
